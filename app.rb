588cc29f20ea047b30730508975953f77c68740e
techTomita <tech.tomita@gmail.com>
2022-06-04 02:07:23 +0000

    登録内容の表示を追加

--- app.rb
+++ app.rb
@@ -4,11 +4,21 @@ require "mysql2.rb"
 
 get "/" do
     client = Mysql2::Client.new(host: "localhost", username: "dbuser", password: "dbpass", database: "booklist", encording: "utf8")
+    
     @records = client.query("select * from books order by created_at desc")
+    
     erb :booklist
 end
 
 post "/" do
+    client = Mysql2::Client.new(host: "localhost", username: "dbuser", password: "dbpass", database: "booklist", encording: "utf8")
+    
+    book_title = params["book_title"]
+    statement = client.prepare("insert into books (book_title) values(?)")
+    statement.execute(book_title)
+    
+    @records = client.query("select * from books order by created_at desc")
+    
     erb :booklist
 end
 # サーバの起動
