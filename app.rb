8974bc91fe49becd5b2b9d1a1ecbabb43d8a1759
techTomita <tech.tomita@gmail.com>
2022-06-04 01:56:57 +0000

    データベースとの接続設定

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 # HTTPリクエストが来た時のWebアプリの処理
 require "sinatra.rb"
+require "mysql2.rb"
 
 get "/" do
+    client = Mysql2::Client.new(host: "localhost", username: "dbuser", password: "dbpass", database: "booklist", encording: "utf8")
+    @records = client.query("select * from books order by created_at desc")
     erb :booklist
 end
 
